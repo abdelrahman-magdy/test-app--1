ad725cb71ed6482040a23807325dfb10a1d410b9
NDP\T-Amohammed <abdelrahman.magdy.k@outlook.com>
2023-06-14 19:56:19 +0300

    revamp code

--- src/Game/index.tsx
+++ src/Game/index.tsx
@@ -1,86 +1,113 @@
-import React, { useEffect, useRef } from 'react'
+import { useRef, useState, useEffect } from 'react';
 import SRR from './shadow_dog.png'
-const playerImage = new Image()
-playerImage.src = SRR
-const spriteWidth:number = 575
-const spriteHeight:number = 523
-const animationState = [
-  {
-    name: 'idle',
-    frames: 7
-  },
-  {
-    name: 'jump',
-    frames: 7
-  },
-  {
-    name: 'fall',
-    frames: 7
-  },
-  {
-    name: 'fun',
-    frames: 7
-  },
-]
+import './style.css'
 
-const spriteAnimation : any = {}; 
+type AnimationFrame = {
+  x: number;
+  y: number;
+};
 
-animationState.forEach((state,index)=>{
-  let frames:any = {
-    loc:[],
-  }
+type AnimationState = {
+  name: string;
+  frames: number;
+}
 
-  for (let j = 0; j < state.frames; j++) {
-    let positionX = j * spriteWidth;
-    let positionY = index * spriteHeight
-    frames.loc.push({
-      x: positionX,
-      y:positionY
-    })
-  }
-  spriteAnimation[state.name] = frames
-})
+type SpriteAnimation = Record<string, { loc: AnimationFrame[] }>;
 
-let gameFrame = 0;
-let staggerframes = 5;
+const playerImage = new Image();
+playerImage.src = SRR;
 
-export default function Game() {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null)
-  let CANVES_WIDTH = 600
-  let CANVAS_HEIGHT = 600
-    let playerState = 'idle';
-    useEffect(() => {
-      const canvas = canvasRef.current;
-      if (!canvas) return
-      const ctx = canvas.getContext('2d');
-      if (!ctx) return
-      // ctx.fillStyle='blue'
+const spriteWidth = 575;
+const spriteHeight = 523;
 
+const animationState: AnimationState[] = [
+  { name: 'idle', frames: 7 },
+  { name: 'jump', frames: 7 },
+  { name: 'fall', frames: 7 },
+  { name: 'fun', frames: 7 },
+  { name: 'dizzy', frames: 11 },
+  { name: 'sit', frames: 5 },
+  { name: 'roll', frames: 7 },
+  { name: 'bite', frames: 7 },
+  { name: 'ko', frames: 12 },
+  { name: 'getHit', frames: 4 },
+];
+
+const createSpriteAnimation = (spriteWidth: number, spriteHeight: number, animationState: AnimationState[]): SpriteAnimation => {
+  const spriteAnimation: SpriteAnimation = {};
+
+  animationState.forEach((state, index) => {
+    const frames: { loc: AnimationFrame[] } = { loc: [] };
+
+    for (let j = 0; j < state.frames; j++) {
+      const positionX = j * spriteWidth;
+      const positionY = index * spriteHeight;
+      frames.loc.push({
+        x: positionX,
+        y: positionY,
+      });
+    }
       
-      // ctx.fillRect(0,0,CANVES_WIDTH,CANVAS_HEIGHT)
+    spriteAnimation[state.name] = frames;
+  });
 
+  return spriteAnimation;
+};
 
-      // after 2d
+const spriteAnimation = createSpriteAnimation(spriteWidth, spriteHeight, animationState);
 
+const animatePlayer = (
+  ctx: CanvasRenderingContext2D | null,
+  playerImage: HTMLImageElement,
+  spriteWidth: number, 
+  spriteHeight: number,
+  spriteAnimation: SpriteAnimation,
+  playerState = 'dizzy'
+) => {
+  let gameFrame = 0;
+  const staggerframes = 5;
 
+  function animate() {
+    if (!ctx) return;
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-      function animate() {
-        ctx?.clearRect(0,0,CANVES_WIDTH,CANVAS_HEIGHT)
-        let position = Math.floor(gameFrame / staggerframes) % spriteAnimation[playerState].loc.length
-        let frameX = spriteWidth * position;
-        let frameY = spriteAnimation[playerState].loc.length.y;
-        ctx?.drawImage( playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    const position = Math.floor(gameFrame / staggerframes) % spriteAnimation[playerState].loc.length;
+    const frameX = spriteWidth * position;
+    const frameY = spriteAnimation[playerState].loc[position].y;
 
-        gameFrame++;
-        requestAnimationFrame(animate);
-        console.log(gameFrame)
-      }
-      animate()
+    ctx.drawImage(
+      playerImage,
+      frameX,
+      frameY,
+      spriteWidth,
+      spriteHeight,
+      0,
+      0,
+      spriteWidth,
+      spriteHeight
+    );
 
-    }, [])
-    
+    gameFrame++;
+    requestAnimationFrame(animate);
+  }
 
-    return (
-        <canvas ref={canvasRef} width={CANVES_WIDTH} height={CANVAS_HEIGHT} />
-    )
+  playerImage.onload = () => {
+    animate();
+  };
+}
+
+export default function Game(): JSX.Element {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [playerState, setPlayerState] = useState('roll');
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    animatePlayer(ctx, playerImage, spriteWidth, spriteHeight, spriteAnimation, playerState);
+  }, [playerState]);
+
+  return <canvas ref={canvasRef} width={600} height={600} />;
 }
